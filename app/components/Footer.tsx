bcda519cb5cb5292ccff98d1b89c703ac976cce8
Tanzil449 <tanzilulalam@gmail.com>
2026-09-21 09:42:36 +1000

    Update footer for Assessment 2

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer>
-      <p>Assessment 1 — Frontend Design and Usability</p>
+      <p>Assessment 2 – Backend, Database and Deployment</p>
       <p>Md Tanzil Ul Alam | Student Number: 21373248</p>
     </footer>
   );
